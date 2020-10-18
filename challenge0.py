140247c6f01345a0d0551c8b698cb642799ffcb3
Syed Hasan Ahmed <hasanahmed999@gmail.com>
2020-10-17 21:10:53 -0400

    Modified file names and Level 1 solution

---
+++ challenge0.py
@@ -0,0 +1,6 @@
+# Paste this result into the URL http://pythonchallenge.com/pc/def/_.html
+
+URL='http://pythonchallenge.com/pc/def/{value}.html'.format(value=2**38)
+print(URL)
+
+# Now you can go to http://pythonchallenge.com/pcc/def/map.html to view solution to level 0 on the wiki
--- challenge1.py
+++ challenge1.py
@@ -1,4 +1,36 @@
-# Paste this result into the URL http://pythonchallenge.com/pc/def/_.html
+# This is just a caesar cipher that shifts digits two to the right
+# We must shift it two to the left
+# Only include alphabet characters
 
-URL='http://pythonchallenge.com/pc/def/{value}.html'.format(value=2**38)
-print(URL)
+'''
+Pseudocode
+
+1. check if the char is alphabetical
+2. append or modify current string w/ the new value minus 2
+3. print the final output string 
+4. replace url http://www.pythonchallenge.com/pc/def/map.html with http://www.pythonchallenge.com/pc/def/string.html
+5. or see the result of the printed string
+
+'''
+
+encodedText = 'g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq \
+                glcddgagclr ylb rfyr\'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.'
+decodedText = ''
+for i in range(len(encodedText)):
+    if encodedText[i].isalpha():
+        if encodedText[i] == 'y':
+            decodedText += 'a'
+        elif encodedText[i] == 'z':
+            decodedText += 'b'
+        else:
+            decodedText += chr( ord(encodedText[i]) + 2 )
+    else:
+        decodedText += encodedText[i]
+
+print(decodedText)
+
+# After working on this, string.makestrans would've worked.
+# using this algo on map would make it ocr
+print("http://www.pythonchallenge.com/pc/def/ocr.html")
+
+# Now you can go to http://pythonchallenge.com/pcc/def/ocr.html to view solution to level 1 on the wiki
